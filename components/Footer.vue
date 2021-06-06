597b853f67e7b9bff9741674f5d6d016f247ee31
Artrm Poyarkov <poart1405@gmail.com>
2021-06-06 16:33:19 +0300

    update footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="border-t py-4">
     <div class="container flex md:flex-row flex-col">
       <div class="flex md:flex-col md:w-2/12 w-full mr-6 md:mb-0 mb-3">
-        <img src="https://breeders-zone.s3.us-east-2.amazonaws.com/static/icons/logo.svg" alt="" class="img-fluid md:w-full w-6/12">
+        <img src="https://breeders-zone.s3.us-east-2.amazonaws.com/static/icons/logo.svg" alt="" class="h-auto md:w-full w-6/12">
         <div class="flex items-center justify-center md:w-full  w-6/12">
           <FontAwesomeIcon class="md:text-2xl text-3xl mr-3" :icon="['fab', 'vk']" style="color: #2787F5;"/>
           <FontAwesomeIcon class="md:text-2xl text-3xl mr-3" :icon="['fab', 'youtube']" style="color: #ff0000;"/>
