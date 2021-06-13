3e8489aaaec60195f238bbe71d14df32148495f4
Artrm Poyarkov <poart1405@gmail.com>
2021-06-13 13:11:39 +0300

    update product card

--- components/Product/Card.vue
+++ components/Product/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col lg:w-3/12 md:w-4/12 sm:w-6/12 w-full px-2 mb-2">
+  <div class="flex flex-col lg:w-3/12 md:w-4/12 sm:w-6/12 w-full md:px-2 mb-2">
     <div class="flex flex-col shadow rounded-xl bg-white p-2 flex-1">
       <NuxtLink :to="'/category/' + product.kind.slug + '/' + product.id"  class="relative mb-2">
         <div class="absolute bg-white shadow rounded top-2 right-2 p-1">
