282b448d5baf1821e6ee742d20f31e3e8ceb61f8
Artrm Poyarkov <poart1405@gmail.com>
2021-06-13 13:12:22 +0300

    update morphs select component

--- components/MorphsSelect.vue
+++ components/MorphsSelect.vue
@@ -45,18 +45,12 @@
 <script lang="ts">
   import Vue from 'vue';
   import {mapGetters} from 'vuex';
-  import VueSelect from "vue-select";
   import _ from "lodash";
   import {IKind, IMorph} from "~/types";
-  import {formatMorph, formatMorphClass} from "~/utils";
-  import {createPopper} from "~/node_modules/@popperjs/core";
+  import {formatMorph, formatMorphClass, withPopper} from "~/utils";
 
   export default Vue.extend({
     name: "MorphsSelect",
-
-    components: {
-      VueSelect
-    },
 
     data() {
       return {
@@ -106,48 +100,7 @@
           })
       }, 450),
 
-      withPopper (dropdownList: any, component: any, {width}: any) {
-        /**
-         * We need to explicitly define the dropdown width since
-         * it is usually inherited from the parent with CSS.
-         */
-        dropdownList.style.width = width;
-
-        /**
-         * Here we position the dropdownList relative to the $refs.toggle Element.
-         *
-         * The 'offset' modifier aligns the dropdown so that the $refs.toggle and
-         * the dropdownList overlap by 1 pixel.
-         *
-         * The 'toggleClass' modifier adds a 'drop-up' class to the Vue Select
-         * wrapper so that we can set some styles for when the dropdown is placed
-         * above.
-         */
-        const popper = createPopper(component.$refs.toggle, dropdownList, {
-          placement: 'bottom',
-          modifiers: [
-            {
-              name: 'offset', options: {
-                offset: [0, -1]
-              }
-            },
-            {
-              name: 'toggleClass',
-              enabled: true,
-              phase: 'write',
-              fn ({state}) {
-                component.$el.classList.toggle('drop-up', state.placement === 'top')
-              },
-            }]
-        });
-
-        /**
-         * To prevent memory leaks Popper needs to be destroyed.
-         * If you return function, it will be called just before dropdown is removed from DOM.
-         */
-        return () => popper.destroy();
-      },
-
+      withPopper,
       formatMorphClass,
       formatMorph
     },
