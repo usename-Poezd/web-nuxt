4cdf4d36167c59797fc27d977d64841067ead356
Artrm Poyarkov <poart1405@gmail.com>
2021-06-13 13:14:24 +0300

    update types

--- store/core.ts
+++ store/core.ts
@@ -5,31 +5,31 @@ const SET_KINDS = 'SET_KINDS';
 const SET_HEADER_MENU_SHOW = 'SET_HEADER_MENU_SHOW';
 const SET_SCROLL_Y = 'SET_SCROLL_Y';
 
-export type RootState = {
+export type CoreState = {
   kinds: Array<IKind>,
   headerMenuShow: boolean,
   scrollY: number,
 }
 
-export const state = (): RootState => ({
+export const state = (): CoreState => ({
   kinds: [],
   headerMenuShow: false,
   scrollY: 0
 });
 
-export const getters: GetterTree<RootState, RootState> = {
+export const getters: GetterTree<CoreState, CoreState> = {
   activeKinds: (state) => state.kinds.filter( k => k.active),
   kinds: (state) => state.kinds,
   headerMenuShow: state => state.headerMenuShow
 };
 
-export const mutations: MutationTree<RootState> = {
+export const mutations: MutationTree<CoreState> = {
   [SET_KINDS]: (state, kinds) => state.kinds = kinds,
   [SET_HEADER_MENU_SHOW]: (state, value: boolean) => state.headerMenuShow = value,
   [SET_SCROLL_Y]: (state, value: number) => state.scrollY = value,
 };
 
-export const actions: ActionTree<RootState, RootState> = {
+export const actions: ActionTree<CoreState, CoreState> = {
   async load({ commit }) {
     const kinds = await this.$api.getKinds();
     commit(SET_KINDS, kinds)
--- store/index.ts
+++ store/index.ts
@@ -1,13 +1,29 @@
 import {ActionTree} from 'vuex'
+import {AuthState} from "~/store/auth";
+import {CoreState} from "~/store/core";
 
 export const state = () => ({
 
 });
 
-export type RootState = ReturnType<typeof state>
+export type RootState = {
+  auth: AuthState
+  core: CoreState
+};
 
 export const actions: ActionTree<RootState, RootState> = {
-  async nuxtServerInit({ dispatch }) {
-    await dispatch('core/load')
+  async nuxtServerInit({ state, dispatch }) {
+    await dispatch('core/load');
+
+    const { access_token, refresh_token } = state.auth;
+
+    if (access_token || refresh_token) {
+      try {
+        await dispatch('auth/me')
+      } catch (e) {
+        // catch any errors and automatically logout the user
+        await dispatch('auth/logout')
+      }
+    }
   }
 };
