b70b5ee727a297105e3a32e8f0e7a9e87fc12b09
Artrm Poyarkov <poart1405@gmail.com>
2021-06-18 19:51:46 +0300

    update header mobile

--- components/Headers/MobileHeader.vue
+++ components/Headers/MobileHeader.vue
@@ -1,21 +1,30 @@
 <template>
   <nav class="fixed bottom-0 flex bg-white w-full shadow">
-    <a href="" class="flex flex-col items-center w-full px-3 py-2 flex flex-col justify-center text-green-600">
+    <NuxtLink
+      to="/"
+      :class="`flex flex-col items-center w-full px-3 py-2 flex flex-col justify-center ${$route.path === '/' ? 'text-green-600' : 'text-gray-400'}`"
+    >
       <svg class="w-6" version="1.2" baseProfile="tiny" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 3s-6.186 5.34-9.643 8.232c-.203.184-.357.452-.357.768 0 .553.447 1 1 1h2v7c0 .553.447 1 1 1h3c.553 0 1-.448 1-1v-4h4v4c0 .552.447 1 1 1h3c.553 0 1-.447 1-1v-7h2c.553 0 1-.447 1-1 0-.316-.154-.584-.383-.768-3.433-2.892-9.617-8.232-9.617-8.232z"></path></svg>
       <div class="text-center text-xs font-semibold">Главная</div>
-    </a>
-    <a href="" class="flex flex-col items-center w-full px-3 py-2 flex flex-col justify-center text-gray-400">
+    </NuxtLink>
+    <NuxtLink
+      to="/category"
+      :class="`flex flex-col items-center w-full px-3 py-2 flex flex-col justify-center ${$route.path === '/category' ? 'text-green-600' : 'text-gray-400'}`"
+    >
       <FontAwesomeIcon icon="list" class="text-xl mr-2"/>
       <div class="text-center text-xs font-semibold">Категории</div>
-    </a>
+    </NuxtLink>
     <a href="" class="flex flex-col items-center w-full px-3 py-2 flex flex-col justify-center text-gray-400">
       <FontAwesomeIcon icon="comments"/>
       <div class="text-center text-xs font-semibold">Чат</div>
     </a>
-    <a href="" class="flex flex-col items-center w-full px-3 py-2 flex flex-col justify-center text-gray-400">
+    <NuxtLink
+      :to="isAuthenticated && user ? '/profile' : '/auth/login'"
+      :class="`flex flex-col items-center w-full px-3 py-2 flex flex-col justify-center ${$route.path.includes('/profile') ? 'text-green-600' : 'text-gray-400'}`"
+    >
       <FontAwesomeIcon icon="user-alt" class="text-xl"/>
-      <div class="text-center text-xs font-semibold">{{isAuthenticated ? user.name : 'Профиль'}}</div>
-    </a>
+      <div class="text-center text-xs font-semibold">{{isAuthenticated && user ? user.name : 'Профиль'}}</div>
+    </NuxtLink>
   </nav>
 </template>
 
