ab2581e2d6a0b3bf95f6478803d4daabfb6b1650
Artrm Poyarkov <poart1405@gmail.com>
2021-06-26 20:32:53 +0300

    create currency input

---
+++ components/Forms/CurrencyInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <input
+    ref="inputRef"
+    :value="formattedValue"
+  >
+</template>
+
+<script>
+import useCurrencyInput from 'vue-currency-input'
+import {defineComponent} from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  props: {
+    value: Number, // Vue 2: value
+    options: Object
+  },
+
+  setup (props) {
+    const {
+      inputRef,
+      formattedValue
+    } = useCurrencyInput(props.options)
+
+    return { inputRef, formattedValue }
+  }
+})
+</script>
