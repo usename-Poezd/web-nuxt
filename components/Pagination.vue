549394be2b01c1c6f8d59b4d63389c234fb7579e
Artrm Poyarkov <poart1405@gmail.com>
2021-06-26 20:34:03 +0300

    fix pagination

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -1,44 +1,84 @@
 <template>
   <ul v-if="currentPage !== lastPage" class="flex items-center border-t mt-3">
     <li v-if="currentPage > 1">
-      <a href="/catalog/literatura-i-knigi/" id="navigation_1_previous_page" class="block">
+      <NuxtLink
+        :to="{
+          path: $route.path,
+          query: {
+            ...$route.query,
+            page: currentPage - 1
+          }
+        }"
+      >
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 relative top-0.5 text-gray-400">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
         </svg>
-      </a>
+      </NuxtLink>
     </li>
-
-
-    <li class="relative">
-      <span class="w-full absolute t-0 h-0.5 bg-green-700"></span>
-      <span class="p-3 text-green-700 font-semibold block hover:no-underline">1</span>
-    </li>
-
-    <li>
-      <a class="p-3 text-gray-400 font-semibold block hover:no-underline" href="/catalog/literatura-i-knigi/?PAGEN_1=2">2</a>
+    <li v-for="page in pages"  class="relative">
+      <span v-if="page === currentPage" class="w-full absolute t-0 h-0.5 bg-green-700"></span>
+      <NuxtLink
+        :to="{
+          path: $route.path,
+          query: {
+            ...$route.query,
+            page
+          }
+        }"
+        :class="`p-3 ${page === currentPage ? 'text-green-700 ' : 'text-gray-400'} font-semibold block hover:no-underline`"
+      >
+        {{page}}
+      </NuxtLink>
     </li>
 
     <li v-if="currentPage !== lastPage">
-      <a href="/catalog/literatura-i-knigi/?PAGEN_1=2" class="block">
+      <NuxtLink
+        :to="{
+          path: $route.path,
+          query: {
+            ...$route.query,
+            page: currentPage + 1
+          }
+        }"
+      >
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 relative top-0.5 text-gray-400">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
         </svg>
-      </a>
+      </NuxtLink>
     </li>
   </ul>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-  import {RUB, getPrice} from "~/utils";
+import Vue, { PropType } from 'vue';
 
-  export default Vue.extend({
-    name: "Pagination",
-
-
-    props: {
-      currentPage: Number,
-      lastPage: Number
+export default Vue.extend({
+  props: {
+    currentPage: {
+      type: Number as PropType<number>,
+      default: 1
+    },
+    lastPage: {
+      type: Number as PropType<number>,
+      default: 1
     }
-  });
+  },
+
+  data() {
+    const pageRange = 2;
+
+    const start = (this.currentPage - pageRange) > 0 ? this.currentPage - pageRange : 1;
+    const end = (this.currentPage + pageRange) < this.lastPage ? this.currentPage + pageRange : this.lastPage;
+
+    const pages = []
+    for (let i = start; i <= end; i++) {
+      pages.push(i)
+    }
+
+    return {
+      pages: pages
+    }
+  }
+});
 </script>
+
