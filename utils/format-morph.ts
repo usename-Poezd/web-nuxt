d57a31ee63631d36111b6b261cc2f24eea3b22d1
Artrm Poyarkov <poart1405@gmail.com>
2021-07-03 15:47:42 +0300

    update utils

--- utils/format-morph.ts
+++ utils/format-morph.ts
@@ -1,9 +1,16 @@
 import {IMorph} from "~/types";
 
-const formatMorphClass = (morph: IMorph): string =>
-  morph.trait.trait_group ?
-    `morph-${morph.trait.type}-${morph.trait.trait_group.label}`.toLowerCase()
-    : `morph-${morph.trait.type}-${morph.trait.title}`.toLowerCase();
+const formatMorphClass = (morph: IMorph): string => {
+  if ((morph.trait as any).trait_group) {
+    return `morph-${morph.trait.type}-${(morph.trait as any).trait_group.label}`.toLowerCase()
+  }
+
+  if (morph.trait.traitGroup) {
+    return `morph-${morph.trait.type}-${morph.trait.traitGroup.label}`.toLowerCase()
+  }
+
+  return `morph-${morph.trait.type}-${morph.trait.title}`.toLowerCase()
+};
 
 const formatMorph = (morph: IMorph): string =>
   morph.trait.title === 'Visual' || morph.trait.title === 'Normal' ?
--- utils/format-price.ts
+++ utils/format-price.ts
@@ -1,3 +1,7 @@
 import currency from "currency.js"
 
 export const RUB = (number: number) => currency(number, { symbol: '', decimal: ',', separator: ' ', precision: 0 }).format() + ' ₽';
+
+export const USD = (number: number) => currency(number, { symbol: '', decimal: '.', separator: ' ', precision: 2 }).format() + ' US$';
+
+export const EUR = (number: number) => currency(number, { symbol: '', decimal: '.', separator: ' ', precision: 2 }).format() + '€';
