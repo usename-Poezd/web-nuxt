bd7bb2744189ece2ed89ee0d1dc7771e32429814
Artrm Poyarkov <poart1405@gmail.com>
2021-07-03 15:44:11 +0300

    create documents

---
+++ pages/documents/_slug.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container md:flex">
+    <div class="w-3/12 md:block hidden mr-4 border-r py-3">
+      <ul>
+        <li v-for="sideDocument in documents" :key="`document-${sideDocument.id}`">
+          <NuxtLink
+            :to="`/documents/${sideDocument.label}`"
+            :class="`relative block px-4 py-4 transition hover:bg-gray-100 rounded-l-md ${document.id === sideDocument.id && 'text-green-600'}`"
+          >
+            {{ sideDocument.title }}
+            <span v-if="document.id === sideDocument.id" class="block h-full w-1 absolute top-0 right-0 bg-green-600"></span>
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+    <div class="md:w-9/12 w-ful py-7">
+      <NuxtLink to="/documents" class="md:hidden flex items-center mb-4">
+        <FontAwesomeIcon icon="chevron-left" size="lg" class="md:hidden block mr-2"/>
+        <span class="font-semibold">Назад</span>
+      </NuxtLink>
+      <h1 class="font-bold text-3xl mb-14">{{document.title}}</h1>
+      <div class="document" v-html="document.description">
+        {{document.description}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data: () => ({
+    documents: [],
+    document: {}
+  }),
+
+  async asyncData({params, $api}) {
+    const documents = await $api.getDocuments();
+    const document = await $api.getDocument(params.slug);
+
+    return {
+      documents,
+      document
+    }
+  }
+})
+</script>
+
---
+++ pages/documents/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container md:flex">
+    <div class="md:w-3/12 w-full md:mr-4 md:border-r py-3">
+      <h1 class="text-2xl font-bold md:hidden mb-4">Юридические документы</h1>
+      <ul>
+        <li v-for="document in documents" :key="`document-${document.id}`">
+          <NuxtLink
+            :to="`/documents/${document.label}`"
+            class="flex items-center md:px-4 py-4 transition hover:bg-gray-100 rounded-l-md"
+          >
+            <span class="md:full w-11/12 md:mr-0 mr-2">{{ document.title }}</span>
+            <FontAwesomeIcon icon="arrow-right" size="lg" class="md:hidden w-1/12 text-green-600"/>
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+    <div class="md:block hidden w-9/12 flex py-7">
+      <div class="text-center m-auto">
+        <FontAwesomeIcon icon="bookmark" size="4x" class="text-yellow-400 mb-4"/>
+        <p class="font-semibold">Выберете документ для ознокомления</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data: () => ({
+    documents: []
+  }),
+
+  async asyncData({$api}) {
+    const documents = await $api.getDocuments();
+
+    return {
+      documents
+    }
+  }
+})
+</script>
+
