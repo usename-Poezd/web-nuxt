96190bd0d6aeb5dbc05c40cb0b75e677f7677922
Artrm Poyarkov <poart1405@gmail.com>
2021-07-10 13:18:54 +0300

    add ts

--- main.d.ts
+++ main.d.ts
@@ -1,5 +1,10 @@
 declare module 'vue-textarea-autosize';
 
+declare module "~/*" {
+  import Vue from 'vue'
+  export default Vue
+}
+
 declare module "vue-js-modal/dist/ssr.nocss" {
   import VueJSModal from "vue-js-modal";
 
