52080d73bd2593411ebbc5ed9b8897ab10853632
Artrm Poyarkov <poart1405@gmail.com>
2021-08-18 11:29:43 +0300

    delete plugins

--- plugins/vue-select.ts
+++
@@ -1,4 +0,0 @@
-import VueSelect from "vue-select";
-import Vue from "vue";
-
-Vue.component("VueSelect", VueSelect);
--- plugins/vue2-datepicker.ts
+++
@@ -1,6 +0,0 @@
-import Vue from "vue";
-import DatePicker from 'vue2-datepicker';
-
-import 'vue2-datepicker/locale/ru';
-// @ts-ignore
-Vue.component("DatePicker", DatePicker);
